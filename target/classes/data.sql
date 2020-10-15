549a225f6abdc4aab378f4e8de5f33d4984af687
Harshitha <harshitha711@gmail.com>
2020-10-14 21:42:53 -0600

    Clean up unwanted file

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-insert into item (name, price, description) values ('Round Widget', 2.99, 'A widget that is round');
-insert into item (name, price, description) values ('Square Widget', 1.99, 'A widget that is square');
-insert into item (name, price, description) values ('2T Widget', 1.98, 'A widget that is 2T');
